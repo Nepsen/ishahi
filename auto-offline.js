58dffc691eb0ba847c014a39b9a8cfb4524b716c
Nepsen <arafatislamlam15@gmail.com>
2025-08-09 22:21:13 +0600

    Update auto-offline.js

--- auto-offline.js
+++ auto-offline.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/ishahi/auto-offline.js')
+  navigator.serviceWorker.register('/ishahi/offline-cache.js')
     .then(() => console.log('Offline service worker registered'))
     .catch(e => console.error('Service worker registration failed:', e));
 }
